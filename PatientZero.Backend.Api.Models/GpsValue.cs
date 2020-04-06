3030a130c131fc47a5b74f395d6e39ac4a2a968f
Machiel Visser <azure@machielvisser.com>
2020-04-06 10:05:47 +0200

    Add project files.

---
+++ PatientZero.Backend.Api.Models/GpsValue.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace PatientZero.Backend.Api.Models
+{
+    public class GpsValue
+    {
+        [JsonProperty("tracker_id")]
+        public int TrackerId { get; set; }
+        [JsonProperty("altitude")]
+        public float Altitude { get; set; }
+        [JsonProperty("lat")]
+        public float Latitude { get; set; }
+        [JsonProperty("lng")]
+        public float Longitude { get; set; }
+        [JsonProperty("angle")]
+        public float Angle { get; set; }
+        [JsonProperty("speed")]
+        public float Speed { get; set; }
+        [JsonProperty("accuracy")]
+        public float Accuracy { get; set; }
+        [JsonProperty("dt")]
+        public float Timestamp { get; set; }
+    }
+}
---
+++ PatientZero.Backend.Api/Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PatientZero.Backend.Api.Models;
+
+namespace PatientZero.Backend.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        // POST: api/Location
+        [HttpPost]
+        public void Post([FromBody] GpsValue[] positions)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(positions));
+        }
+    }
+}
